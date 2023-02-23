8e720ad2fa9cc577b0c3d00cfe2b207248ed2df1
markov404 <mark.markov.2003@yandex.ru>
2023-02-23 22:32:57 +0300

    Change users.components.wrappers package.

--- users/views.py
+++ users/views.py
@@ -1,26 +1,31 @@
 
 from django.http import HttpResponse
-from django.views.decorators.http import require_http_methods
 from django.core.handlers.wsgi import WSGIRequest
+from django.views.decorators.http import require_http_methods
 
-from .wrappers.wrappers import logged_in_user_only
+from .wrappers.wrappers import not_logged_in_user_only
 from .components.authentification import Authentification
 from .components.authorisation import Authorisation
 
 # Create your views here.
 
 @require_http_methods(["POST"])
-@logged_in_user_only()
+@not_logged_in_user_only()
 def auth(request: WSGIRequest):
     email = request.POST.get("email")
     password = request.POST.get("password")
 
-    auth = Authentification()
-    user = auth.is_user_exists(email)
+    authe = Authentification()
+    user = authe.is_user_exists(email)
+
     if user is not None:
-        if auth.is_password_right(user, password):
-            authorisation = Authorisation()
-            authorisation.login(request, user)
+        if authe.is_password_right(user, password):
+            autho = Authorisation()
+            autho.login(request, user)
+
+            del authe
+            del autho
+
             return HttpResponse("Ты залогинился")
         else:
             return HttpResponse("Не правильный пароль")
--- users/wrappers/wrappers.py
+++ users/wrappers/wrappers.py
@@ -2,13 +2,29 @@
 from functools import wraps
 from django.http import HttpResponse
 
-def logged_in_user_only():
+
+def not_logged_in_user_only():
 
     def decorator(func):
         @wraps(func)
         def inner(request, *args, **kwargs):
             if 'member_id' in request.session:
                 response = HttpResponse("Ты итак уже залогинен")
+                return response
+            return func(request, *args, **kwargs)
+
+        return inner
+
+    return decorator
+
+
+def logged_in_user_only():
+
+    def decorator(func):
+        @wraps(func)
+        def inner(request, *args, **kwargs):
+            if 'member_id' not in request.session:
+                response = HttpResponse("Нельзя")
                 return response
             return func(request, *args, **kwargs)
 
