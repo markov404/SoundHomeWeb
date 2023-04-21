73944cee5c413621928c8020c887583ea7137d07
markov404 <44326964+markov404@users.noreply.github.com>
2023-04-21 21:08:51 +0300

    Update settings.py

--- soundhome/settings.py
+++ soundhome/settings.py
@@ -67,17 +67,7 @@ DOT_ENV_PATH = f'{BASE_DIR}/configuration.env'
 if os.path.exists(DOT_ENV_PATH):
     if not load_dotenv(DOT_ENV_PATH):
        log.exception('Environmental variables is not set upped!')
-    
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y09n_6*6$%a70ti#jo5d7e$_nmee=v$=r8fd5aly%70lfr0mvw'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -135,18 +125,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'soundhome.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
@@ -199,3 +177,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = f'{BASE_DIR}/media'
 MEDIA_URL = '/media/'
+
+
+try:
+    from soundhome.local_settings import *
+except ImportError:
+    from soundhome.prod_settings import *
